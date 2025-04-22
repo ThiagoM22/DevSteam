3378404ce8e9712fd7915a0da391d1bcd544803a
Thiago Mazzi <thiagodev728@gmail.com>
2025-04-22 14:17:10 -0300

    promocard finalizado

--- src/components/Promotion.jsx
+++ src/components/Promotion.jsx
@@ -5,8 +5,11 @@ const Promotion = () => {
   return (
     <div id="promotion">
       <h2>Promoções</h2>
-      <div id="itenspromo">
+      <div id="itenspromo" className="d-flex flex-wrap gap-4 justify-content-around">
         <PromoCard/>
+        <PromoCard/>
+        <PromoCard/>
+
       </div>
     </div>
   );
