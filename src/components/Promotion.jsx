3a977905cd51b722e566a17e3e769b37148d0cd1
Thiago Mazzi <thiagodev728@gmail.com>
2025-04-22 11:27:05 -0300

    Ajustes do promocard

--- src/components/Promotion.jsx
+++ src/components/Promotion.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import PromoCard from "./PromoCard";
 
 const Promotion = () => {
   return (
     <div id="promotion">
       <h2>Promoções</h2>
       <div id="itenspromo">
-        
+        <PromoCard/>
       </div>
     </div>
   );
